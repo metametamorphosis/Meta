8c242910b01b8ea8e59ee047c6e1a751ae4da98f
metametamorphosis <138376951+metametamorphosis@users.noreply.github.com>
2023-07-22 09:39:46 +0300

    merge sort testing

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,61 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        var primes = getPrimes(100);
-        System.out.println(primes.get(primes.size() - 1));
+        int[] arr = new int[5];
+
+        for (int i = 0, n = arr.length; i < n; i++) {
+            arr[i] = new Random().nextInt(49) + 1;
+        }
+
+        vyvod(arr);
+
+        mergeSort(arr);
+
+        vyvod(arr);
     }
-    public static List<Integer> getPrimes(int count) {
-        List<Integer> primes = new ArrayList<>();
-        if (count > 0) {
-            primes.add(2);
-            for (int i = 3; primes.size() < count; i += 2) {
-                if (isPrime(i, primes)) primes.add(i);
+
+    private static void mergeSort(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return;
+
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++)
+            l[i] = arr[i];
+        for (int i = mid; i < n; i++)
+            r[i - mid] = arr[i];
+
+        mergeSort(l);
+        mergeSort(r);
+        merge(arr, l, r);
+    }
+
+    private static void merge(int[] arr, int[] l, int[] r) {
+        int left = l.length;
+        int right = r.length;
+        int i = 0, j = 0, idx = 0;
+
+        while (i < left && j < right) {
+            if (l[i] < r[j]) {
+                arr[idx++] = l[i++];
+            } else {
+                arr[idx++] = r[j++];
             }
         }
-        return primes;
-    }
-    public static boolean isPrime (int number, List<Integer> primes) {
-        double sqrt = Math.sqrt(number);
-        for (int n: primes) {
-            if (n > sqrt) return true;
-            if (number % n == 0) return false;
+
+        while (i < left) {
+            arr[idx++] = l[i++];
         }
-        return true;
+        while (j < right) {
+            arr[idx++] = r[j++];
+        }
+    }
+
+    private static void vyvod(int[] arr) {
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.print("\n");
     }
 }
