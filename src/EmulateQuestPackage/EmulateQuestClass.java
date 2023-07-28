393e77b3fda2f2bc3af9b66d52e6ee27bbcf1d6d
metametamorphosis <138376951+metametamorphosis@users.noreply.github.com>
2023-07-28 12:14:56 +0300

    made a method isPalindrome: "Казак" - TRUE "А роза упала на лапу Азора" - TRUE "хехе_А роза упала на лапу Азора_хехе" - FALSE "ехех777А роза упала на лапу%%№. Азора_хехе" - TRUE

--- src/EmulateQuestPackage/EmulateQuestClass.java
+++ src/EmulateQuestPackage/EmulateQuestClass.java
@@ -4,30 +4,47 @@ import java.util.Scanner;
 
 public class EmulateQuestClass {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("fibonacci's edge = ");
-        byte x = scanner.nextByte();
-        System.out.println("_____X_____");
-        fibonacci(x);
-    }
-    
-    private static void fibonacci(byte fibedge){
-        if (fibedge < 1 || fibedge > 93) System.out.println("от 1 до 93(предел long)");
-        else if (fibedge == 1) System.out.println(0);
-        else if (fibedge == 2) System.out.println(1);
-        else {
-            long[] array = new long[fibedge];
-            array[0] = 0;
-            System.out.print(array[0] + ", ");
-            array[1] = 1;
-            System.out.print(array[1] + ", ");
 
-            for (byte i=2; i<fibedge-1; i++) {
-                array[i] = array[i-2] + array[i-1];
-                System.out.print(array[i] + ", ");
+        String a = "Казак";                                                         //TRUE
+        String b = "А роза упала на лапу Азора";                                    //TRUE
+        String c = "хехе_А роза упала на лапу Азора_хехе";                          //FALSE
+        String d = "ехех???..?,?,?,--%45777А роза упала на лапу%%№. Азора_хехе";    //TRUE
+        String e = "Do geese see God?";                                             //TRUE
+        String f = "Madam, I'm Adam";                                               //TRUE
+        String g = "нА доМе чемОдан ";                                              //TRUE
+        String h = "нА доМе чемО0дан ";                                             //TRUE
+
+        System.out.printf("%B\n", isPalindrome(a));
+        System.out.printf("%B\n", isPalindrome(b));
+        System.out.printf("%B\n", isPalindrome(c));
+        System.out.printf("%B\n", isPalindrome(d));
+        System.out.printf("%B\n", isPalindrome(e));
+        System.out.printf("%B\n", isPalindrome(f));
+        System.out.printf("%B\n", isPalindrome(g));
+        System.out.printf("%B\n", isPalindrome(h));
+
+    }
+
+    static boolean isPalindrome(String string) {
+        int first = 0;
+        int last = string.length() - 1;
+
+        while (first < last) {
+            if (Character.toLowerCase(string.toCharArray()[first]) == Character.toUpperCase(string.toCharArray()[first])) {
+                first++;
+                continue;
             }
-            array[fibedge-1] = array[fibedge-3] + array[fibedge-2];
-            System.out.println(array[fibedge-1] + ".");
+            if (Character.toLowerCase(string.toCharArray()[last]) == Character.toUpperCase(string.toCharArray()[last])) {
+                last--;
+                continue;
+            }
+            if (Character.toLowerCase(string.toCharArray()[first]) == Character.toLowerCase(string.toCharArray()[last])) {
+                first++;
+                last--;
+            } else {
+                return false;
+            }
         }
+        return true;
     }
 }
