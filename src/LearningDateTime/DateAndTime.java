57710e58bd67c0ae0dc653944d523f6642ef33fe
metametamorphosis <138376951+metametamorphosis@users.noreply.github.com>
2023-09-01 21:46:34 +0300

    Изучил): Что такое LocalDateTime, LocalDate, LocalTime, как создаются (не через конструктор, а через статик метод of),добавление, отнимание дат и времени, а также работу с классом Период

--- src/LearningDateTime/DateAndTime.java
+++ src/LearningDateTime/DateAndTime.java
@@ -2,9 +2,34 @@ package LearningDateTime;
 import java.time.*;
 
 public class DateAndTime {
+    //LocalDate, LocalTime, LocalDateTime are IMMUTABLE!!!
     public static void main(String[] args) {
-        System.out.println(LocalDate.now());
-        System.out.println(LocalTime.now());
-        System.out.println(LocalDateTime.now());
+        LocalDate ld = LocalDate.of(2023, Month.SEPTEMBER, 1); // (2023, 9, 1)
+        System.out.printf("Local Date: %s\n", ld);
+
+        LocalTime lt = LocalTime.of(1, 59, 59, 999999999);
+        System.out.printf("Local Time: %s\n", lt);
+
+        LocalDateTime ldt = LocalDateTime.of(ld, lt);
+        System.out.printf("Local DateTime: %s\n", ldt);
+
+        System.out.println("___");
+        System.out.println("LearningPlusMinus");
+
+        ldt = ldt.plusNanos(1).minusMonths(8).minusMonths(36);
+        System.out.printf("Local DateTime: %s\n", ldt);
+
+        System.out.println("___");
+
+        //isAfter, isBefore (equals same type objects)
+        System.out.println("LearningPeriod");
+        LocalDate periodStart = LocalDate.of(2000, Month.SEPTEMBER, 1);
+        LocalDate periodEnd = LocalDate.of(2007, Month.SEPTEMBER, 1);
+        Period period = Period.of(1, 8, 15);
+        while (periodStart.isBefore(periodEnd)) {
+            System.out.println(periodStart);
+            periodStart = periodStart.plus(period);
+        }
+
     }
 }
