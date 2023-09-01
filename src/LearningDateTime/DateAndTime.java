4af316afe4055412b77552eb82b90622d8f18590
metametamorphosis <138376951+metametamorphosis@users.noreply.github.com>
2023-09-01 20:14:33 +0300

    Базовые методы/Вспоминаю, как работать с гит(посредством интерфейса Инт. Идеи

---
+++ src/LearningDateTime/DateAndTime.java
@@ -0,0 +1,10 @@
+package LearningDateTime;
+import java.time.*;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
